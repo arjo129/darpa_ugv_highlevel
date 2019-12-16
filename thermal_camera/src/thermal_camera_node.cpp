c0395c9a9eb77385d59b6bf07834b50387bc26be
jing-xuan <tayjxn@gmail.com>
2019-12-16 11:25:36 +0800

    modified template

--- thermal_camera/src/thermal_camera_node.cpp
+++ thermal_camera/src/thermal_camera_node.cpp
@@ -11,14 +11,21 @@ cv::Mat getThermalImage() {
 int main(int argc, char** argv) {
     ros::init(argc, argv, "thermal_camera_driver");
     ros::NodeHandle nh;
-    cv_bridge::CvImagePtr cv_ptr;
+    cv_bridge::CvImagePtr cv_ptr1;
+    cv_bridge::CvImagePtr cv_ptr2;
     image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise("thermal/image_raw", 1);
+    image_transport::Publisher pub1 = it.advertise("thermal/image_raw1", 1);
+    image_transport::Publisher pub2 = it.advertise("thermal/image_raw2", 1);
     ros::Rate rate(10); // Adjust frequency at which you read camera
 
     while(ros::ok()){
         rate.sleep();
-        cv_ptr->image = getThermalImage();
-        pub.publish(cv_ptr->toImageMsg());
+	Mat img1(160, 120, CV_8UC3, Scalar(0, 0, 0));
+	Mat img2(160, 120, CV_8UC3, Scalar(0, 0, 0));
+	getThermalImage(img1, img2);
+        cv_ptr1->image = img1;
+	cv_ptr2->image = img2;
+        pub1.publish(cv_ptr1->toImageMsg());
+	pub2.publish(cv_ptr2->toImageMsg());
     }
 }
