1fea5015376a651003778c1dc80392418c82aaa0
Arjo <arjo129@gmail.com>
2019-12-30 12:06:21 +0800

    updated linking and wrote a simple laserscan sender

---
+++ data_compresor/src/sender.cpp
@@ -0,0 +1,59 @@
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <sensor_msgs/LaserScan.h>
+#include <data_compressor/devices/laserscan.h>
+#include <wireless_msgs/LoraPacket.h>
+
+class CompressedTelemetrySender {
+
+    ros::Publisher loraPub;
+    ros::Subscriber laserscan;
+    data_compressor::LaserScanCompressor scanCompressor;
+    data_compressor::PacketSendQueue sendQueue;
+    std::vector<wireless_msgs::LoraPacket> latestPacket;
+    tf::TransformListener* tfListener;
+
+    void compressScan(sensor_msgs::LaserScan scan){
+        data_compressor::DataPacket pkt = scanCompressor.compress(scan);
+        tf::StampedTransform trans;
+        try {
+            this->tfListener->lookupTransform("odom", "base_link", scan.header.stamp, trans);
+            pkt.estimated_pose.position.x = trans.getOrigin().x();
+            pkt.estimated_pose.position.y = trans.getOrigin().y();
+            pkt.estimated_pose.position.z = trans.getOrigin().z();
+            pkt.estimated_pose.orientation.x = trans.getRotation().x();
+            pkt.estimated_pose.orientation.y = trans.getRotation().y();
+            pkt.estimated_pose.orientation.z = trans.getRotation().z();
+            pkt.estimated_pose.orientation.w = trans.getRotation().w();
+            data_compressor::ByteStream bs = pkt.serialize();
+            this->latestPacket = sendQueue.send(bs,"base_station");
+        } catch (tf::TransformException ex) {
+
+        }
+    }
+
+public:
+    CompressedTelemetrySender(ros::NodeHandle& nh, std::string address): sendQueue(address) {
+        this->laserscan = nh.subscribe("/husky1/scan", 10, &CompressedTelemetrySender::compressScan, this);   
+        this->loraPub = nh.advertise<wireless_msgs::LoraPacket>("/husky1/tx", 10);
+        this->tfListener = new tf::TransformListener();
+    }
+
+    void publish(){
+        for(wireless_msgs::LoraPacket& pkt: this->latestPacket){
+            this->loraPub.publish(pkt);
+        }
+    }
+};
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "compression_node");
+    ros::NodeHandle nh;
+    CompressedTelemetrySender telemetry(nh, "husky1");
+    ros::Rate rate(0.5);
+    while(ros::ok()){
+        ros::spinOnce();
+        telemetry.publish();
+        rate.sleep();
+    }
+    return 0;
+}
