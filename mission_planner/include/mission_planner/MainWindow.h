1133aa1312fae6f3840eafae8ff0e6e5939e1d1a
Arjo <arjo129@gmail.com>
2020-01-09 11:02:03 +0800

    update GUI to allow live map edits

--- mission_planner/include/mission_planner/MainWindow.h
+++ mission_planner/include/mission_planner/MainWindow.h
@@ -4,17 +4,29 @@
 #include <mission_planner/RosThread.h>
 #include <QPixmap>
 #include "ui_MainWindow.h"
+enum class SliderState {
+    EDITING,
+    LIVE_VIEW
+};
 
+enum class EditorState {
+    MOVE,
+    ROTATE
+};
 class MainWindow : public QMainWindow {
     private:
         Ui::MainWindow* ui;
         QGraphicsScene* scene;
         ros::NodeHandle nh;
         ROSThread rosthread;
+        std::vector<QGraphicsPixmapItem*> laserscans;
+        SliderState sliderState;
+        EditorState editorState;
     public:
         MainWindow(ros::NodeHandle nh);
         ~MainWindow();
     public slots:
         void addPixmap(const QPixmap& map, int x, int y, float theta);
+        void sliderMoved(int value);
 };
 #endif
--- mission_planner/src/MainWindow.cpp
+++ mission_planner/src/MainWindow.cpp
@@ -10,6 +10,9 @@ MainWindow::MainWindow(ros::NodeHandle _nh): nh(_nh), rosthread(_nh) {
     ui->graphicsView->setScene(scene);
     qRegisterMetaType<QPixmap>("QPixmap");
     connect(&rosthread, &ROSThread::scanRecieved, this, &MainWindow::addPixmap);
+    connect(ui->progressSlider, &QSlider::sliderMoved, this, &MainWindow::sliderMoved);
+    this->sliderState = SliderState::LIVE_VIEW;
+    this->editorState = EditorState::MOVE;
 }
 
 MainWindow::~MainWindow() {
@@ -19,7 +22,31 @@ MainWindow::~MainWindow() {
 
 void MainWindow::addPixmap(const QPixmap& map, int x, int y, float theta){
     QGraphicsPixmapItem* item = scene->addPixmap(map);
+    laserscans.push_back(item);
     item->setPos(x,y);
     item->setTransformOriginPoint(map.rect().center());
     item->setRotation(theta*57.29);
+    if(sliderState == SliderState::LIVE_VIEW){
+        ui->progressSlider->setValue(laserscans.size());
+        item->setVisible(true);
+    }
+    else{
+        item->setVisible(false);
+    }
+    ui->progressSlider->setRange(0, laserscans.size());
+}
+
+void MainWindow::sliderMoved(int value) {
+    sliderState = SliderState::EDITING;
+    for (int i = 0; i < value; i++) {
+        laserscans[i]->setVisible(true);
+        laserscans[i]->setFlag(QGraphicsItem::GraphicsItemFlag::ItemIsMovable, false);
+        laserscans[i]->setFlag(QGraphicsItem::GraphicsItemFlag::ItemIsSelectable, false);
+    }
+    for (int i = value; i < laserscans.size(); i++) {
+        laserscans[i]->setVisible(false);
+    }
+    laserscans[value]->setVisible(true);
+    laserscans[value]->setFlag(QGraphicsItem::GraphicsItemFlag::ItemIsMovable, true);
+    laserscans[value]->setFlag(QGraphicsItem::GraphicsItemFlag::ItemIsSelectable, true);
 }
