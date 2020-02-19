ffc7760fcb949bac2505a263614b3db72131196c
Hashir Zahir <hashirsahad@gmail.com>
2020-02-20 03:50:29 +0800

    estop working

--- xbee_estop/Scripts/estopReceive.py
+++ xbee_estop/Scripts/estopReceive.py
@@ -2,6 +2,7 @@
 import rospy
 from std_msgs.msg import String
 from digi.xbee.devices import XBeeDevice
+from digi.xbee.io import IOLine
 
 def estop():
     pub = rospy.Publisher('e_stop', String, queue_size=1)
@@ -13,14 +14,31 @@ def estop():
     print("E-Stop node has started")
 
     while True:
-        message = device.read_data()
-        if message:
-            stringType = str(message.data.decode("utf8"))
-            print(stringType, rospy.get_time())
-            msg = String()
-            msg.data = stringType
-            pub.publish(msg)
-        rospy.sleep(0.01)
+        # message = device.read_data()
+        # if message:
+
+        io_sample = device.read_io_sample()
+        # io_line = IOLine.DIO1_AD1
+        io_lines = []
+
+        io_lines.append(IOLine.DIO0_AD0)
+        io_lines.append(IOLine.DIO1_AD1)
+        io_lines.append(IOLine.DIO2_AD2)
+        io_lines.append(IOLine.DIO3_AD3)
+        io_lines.append(IOLine.DIO4_AD4)
+
+        for io_line in io_lines:
+            if io_sample.has_digital_value(io_line):
+                print("Line: " + str(io_line) + " is HIGH")
+
+        # Check if the IO sample contains the expected IO line and value.
+        # if io_sample.has_digital_value(io_line):
+        #     stringType = str(message.data.decode("utf8"))
+        #     print(stringType, rospy.get_time())
+        #     msg = String()
+        #     msg.data = stringType
+        #     pub.publish(msg)
+        rospy.sleep(1)
 
 if __name__ == '__main__':
     try:
---
+++ xbee_estop/Scripts/xbee_estop.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import serial
+import rospy
+from std_msgs.msg import String
+with serial.Serial('/dev/xbee', 9600, timeout=1) as ser:
+    def reset(msg):
+        ser.write(b'\x7E\x00\x05\x08\x01\x44\x31\x04\x7D')
+        ser.read(40)
+    rospy.init_node("estop_xbee")
+    pub = rospy.Publisher("/e_stop", String)
+    sub = rospy.Subscriber("/start", String, reset)
+    while not rospy.is_shutdown():
+        ser.write(b'\x7E\x00\x04\x08\x01\x49\x53\x5A')
+        if ser.read(28) == b'~\x00\x0b\x88\x01IS\x00\x01\x00\x02\x00\x00\x02\xd5':
+            st = String()
+            st.data = "hi"
+            pub.publish(st)
+
