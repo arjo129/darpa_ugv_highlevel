a91dbbf2b65447ec6a1be92e62e36054aafc9afa
Arjo <arjo129@gmail.com>
2020-02-19 10:11:12 -0800

    update scoring server

---
+++ vehicle_drive/estop.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+ 
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+rospy.init_node("estop_muxer")
+cmd_vel_pub = rospy.Publisher("/controller/cmd_vel", Twist)
+cmd_vel_lister = rospy.Subscriber("/cmd_vel", Twist, cmd_vel_forwarder)
+start_sub = rospy.Subscriber("/start", String, on_estop_cancelled)
+stop_sub = rospy.Subscriber("/e_stop", String, on_estop_triggered)
+global estopped
+estopped = False
+
+def cmd_vel_forwarder(message):
+    global estopped
+    if not estopped:
+        cmd_vel_pub.publish(message)
+
+def on_estop_cancelled(message):
+    global estopped
+    estopped = False
+
+def on_estop_triggered(message):
+    global estopped
+    estopped = True
+
+rospy.spin()
