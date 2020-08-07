16113b1e31ddffb4ead852533800332f6ef2f4e5
Hashir Zahir <hashirsahad@gmail.com>
2020-08-07 21:17:25 +0800

    Fill up graph data structure definitions

--- exploration_goal/include/exploration_goal/frontier_graph.h
+++ exploration_goal/include/exploration_goal/frontier_graph.h
@@ -7,6 +7,8 @@
 #include <graph_msgs/Edges.h>
 
 #include <nanoflann/nanoflann.hpp>
+#include <vector>
+#include <algorithm>
 
 struct PointCloud3d_ {
     std::vector<Eigen::Vector3f> pts;
@@ -31,7 +33,12 @@ typedef nanoflann::KDTreeSingleIndexDynamicAdaptor<
 		3
 		> kd_tree_t;
 
-
+typedef enum GraphNodeExploredState 
+{
+    NODE_UNEXPLORED = 0,
+    NODE_EXPLORING = 1,
+    NODE_EXPLORED = 2
+} GraphNodeExploredState;
 
 class FrontierGraph 
 {
@@ -41,17 +48,65 @@ class FrontierGraph
         ~FrontierGraph();
         FrontierGraph mergeLocalGraph (const FrontierGraph & local_graph);
         graph_msgs::GeometryGraph toMsg();
-        void markAsVisited(int node_idx);
+        void markAsUnExplored(int node_idx);
+        void markAsExploring(int node_idx);
+        void markAsExplored(int node_idx);
 
     private:
-        vector<int> visited;
-        vector<set<int>> adjacency_list; //maybe can use map for O(1) ?
+        std::vector<int> explored_state;
+        std::vector<std::set<int>> adjacency_list;
+        std::vector<geometry_msgs::Point> node_idx_to_3d_point;
         int num_nodes;
 };
 
 
+FrontierGraph::FrontierGraph() : num_nodes(0)
+{}
 
+FrontierGraph::FrontierGraph(graph_msgs::GeometryGraph graph) : num_nodes(graph.nodes.size()),
+                                                                explored_state(num_nodes, NODE_UNEXPLORED),
+                                                                adjacency_list(num_nodes, std::set<int>(0))
+{
+    // 0-based List containing mapping of node index to 3D points.
+    node_idx_to_3d_point = graph.nodes; 
 
+    // Convert vector based adj_list to set based adj_list for easier insertion when merging with other graphs
+    for (int idx=0; idx<num_nodes; idx++)
+    {
+        vector<int> adj_vec = graph.edges[idx];
+        std::copy(adj_vec.begin(), adj_vec.end(), std::inserter(adjacency_list[idx], adjacency_list[idx].end()));
+    }
+}
+
+FrontierGraph::~FrontierGraph()
+{}
+
+FrontierGraph::FrontierGraph mergeLocalGraph (const FrontierGraph & local_graph)
+{
+
+}
+
+FrontierGraph::graph_msgs::GeometryGraph toMsg()
+{
+    graph_msgs::GeometryGraph graph;
+
+    graph.header.frame_id = "world";
+    graph.header.stamp = ros::Time::now();
+
+    graph.nodes = node_idx_to_3d_point;
+    
+    for (int idx=0; idx<num_nodes; idx++)
+    {
+        vector<int> adj_vec(adjacency_list[idx].begin(), adjacency_list[idx].end());
+        graph.edges.push_back(adj_vec);
+    }
+
+    return graph;
+}
+
+FrontierGraph::void markAsUnExplored(int node_idx) {explored_state[node_idx] = NODE_UNEXPLORED;}
+FrontierGraph::void markAsExploring(int node_idx) { explored_state[node_idx] = NODE_EXPLORING; }
+FrontierGraph::void markAsExplored(int node_idx) { explored_state[node_idx] = NODE_EXPLORED; }
 
 
 
