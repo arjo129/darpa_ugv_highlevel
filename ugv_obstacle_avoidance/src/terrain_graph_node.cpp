dace8bc640e3970285721e50854d42ce6c8a73bf
Hashir Zahir <hashirsahad@gmail.com>
2020-08-16 23:21:35 +0800

    Fix minor graph 0,0,0 bug and compilation fix

--- ugv_obstacle_avoidance/src/terrain_graph_node.cpp
+++ ugv_obstacle_avoidance/src/terrain_graph_node.cpp
@@ -677,9 +677,9 @@ void frontierCallBack(const PointCloud::ConstPtr& msg){
         }else{
           if(nodeIdMapping.find(a.first) == nodeIdMapping.end()){
             geometry_msgs::Point tempPt;
-            tempPt.x = 0;
-            tempPt.y = 0;
-            tempPt.z = 0;
+            tempPt.x = current.x;
+            tempPt.y = current.y;
+            tempPt.z = current.z;
             gg.nodes.push_back(tempPt);
             gg.edges.push_back(graph_msgs::Edges());
             nodeIdMapping[a.first] = point_id;
