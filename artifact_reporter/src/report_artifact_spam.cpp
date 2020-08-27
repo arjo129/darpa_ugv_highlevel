58fa259eca0da1fa775fec1e18713491c6ce44e9
Hashir Zahir <hashirsahad@gmail.com>
2020-08-28 00:16:44 +0800

    Fix artifact report spammer and reports every detection to subt comms network

--- artifact_reporter/src/report_artifact_spam.cpp
+++ artifact_reporter/src/report_artifact_spam.cpp
@@ -24,38 +24,39 @@ private:
     subt::CommsClient comms_client;
     geometry_msgs::Point artifact_location;
     subt::ArtifactType artifact_type;
+    std::string artifact_type_str;
     bool have_an_artifact_to_report;
 public:
 
     ArtifactReport(ros::NodeHandle nh_) : nh(nh_), comms_client(ROBOT_NAME), have_an_artifact_to_report(false)
     {
         // found artifacts will be attempted to be sent periodically through a timer
-        timer = nh.createTimer(ros::Duration(1.0), boost::bind(&reportArtifacts, _1, boost::ref(commsClient)));
+        timer = nh.createTimer(ros::Duration(1.0), &ArtifactReport::reportArtifacts, this);
 
         // set up communications with the base station for artifact reporting
-        subt::CommsClient ;
-        comms_client.Bind(&BaseStationCallback, ROBOT_NAME);
+        // comms_client.Bind(&BaseStationCallback, ROBOT_NAME);
 
-        detection_sub = nh.subscribe(DETECTIONS_TOPIC, 1, objectDetectionCb);
+        detection_sub = nh.subscribe(DETECTIONS_TOPIC, 1, &ArtifactReport::objectDetectionCb, this);
     }
 
-    void objectDetectionCb(const object_detection::DetectedObjecstMsgr& msg)
+    void objectDetectionCb(const object_detection::DetectedObjectsMsg& msg)
     {
         have_an_artifact_to_report = true;
 
         // for now only care about first detection
         auto detection = msg.detected_objects_msgs[0];
-        std::string detection_class = detection.class_name.data;
+        std::string temp(detection.class_name.c_str());
+        artifact_type_str = temp;
 
-        if (detection_class == "backpack") {
+        if (artifact_type_str == "backpack") {
             artifact_type = subt::ArtifactType::TYPE_BACKPACK;
-        } else if (detection_class == "rope") {
+        } else if (artifact_type_str == "rope") {
             artifact_type = subt::ArtifactType::TYPE_ROPE;
-        } else if (detection_class == "helmet") {
+        } else if (artifact_type_str == "helmet") {
             artifact_type = subt::ArtifactType::TYPE_HELMET;
-        } else if (detection_class == "phone") {
+        } else if (artifact_type_str == "phone") {
             artifact_type = subt::ArtifactType::TYPE_PHONE;
-        } else if (detection_class == "survivor") {
+        } else if (artifact_type_str == "survivor") {
             artifact_type = subt::ArtifactType::TYPE_RESCUE_RANDY;
         } else {
             have_an_artifact_to_report = false;
@@ -66,7 +67,7 @@ public:
 
     }
 
-    void reportArtifacts(const ros::TimerEvent &, subt::CommsClient & commsClient)
+    void reportArtifacts(const ros::TimerEvent &)
     {
         if (!have_an_artifact_to_report) {
             return;
@@ -89,7 +90,10 @@ public:
         }
 
         // report the artifact
-        commsClient.SendTo(serializedData, subt::kBaseStationName);
+        comms_client.SendTo(serializedData, subt::kBaseStationName);
+        ROS_INFO("%s artifact reported successfully at %f, %f, %f", artifact_type_str.c_str(), artifact_location.x,
+                                                                                       artifact_location.y,
+                                                                                       artifact_location.z);
 
         have_an_artifact_to_report = false;
     }
@@ -103,7 +107,7 @@ int main (int argc, char** argv)
     ros::init(argc, argv, "report_artifact_spam_node");
     ros::NodeHandle nh;
 
-    ArtifactReport artifact_report;    
+    ArtifactReport artifact_report(nh);    
 
     ros::spin();
 }
