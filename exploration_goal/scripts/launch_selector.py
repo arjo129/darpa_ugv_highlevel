46b1576c88a899f7ede3b219f4b4b58297a88ad1
Hashir Zahir <hashirsahad@gmail.com>
2020-08-30 01:07:41 +0800

    Add subt scoring start before launching vehicle

--- exploration_goal/scripts/launch_selector.py
+++ exploration_goal/scripts/launch_selector.py
@@ -3,12 +3,16 @@
 import rospy
 import roslaunch
 from sensor_msgs.msg import BatteryState
+from std_srvs.srv import SetBool, SetBoolRequest
+from std_msgs.msg import Bool
 from rospy.exceptions import ROSException
 
 import os
 
 GUARANTEED_X1_TOPIC = '/X1/battery_state'
 TOPIC_TIMEOUT = 10 # seconds
+SUBT_START_SRV_NAME = "/subt/start"
+
 CURR_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 TEAMBASE_LAUNCH_URI = CURR_PATH + "/launch/teambase.launch"
 X1_LAUNCH_URI = CURR_PATH + "/launch/x1.launch"
@@ -24,7 +28,18 @@ def main():
                 before deciding to launch X1 or teambase".format(GUARANTEED_X1_TOPIC, TOPIC_TIMEOUT))
         rospy.wait_for_message(GUARANTEED_X1_TOPIC, BatteryState, timeout=rospy.Duration(TOPIC_TIMEOUT))
 
-        # X1 topic found, running X1 launch file
+        # X1 topic found
+
+        ## Start Subt Scoring
+        rospy.logwarn("[Launch-Selector] Waiting for /subt/start service to start scoring...")
+        rospy.wait_for_service(SUBT_START_SRV_NAME)
+        subt_start_srv = rospy.ServiceProxy(SUBT_START_SRV_NAME, SetBool)
+        start_req = SetBoolRequest()
+        start_req.data = True
+        subt_start_srv(start_req)
+        rospy.loginfo("[Launch-Selector] Subt Scoring server started")
+
+        ## Start X1 launch
         launch = roslaunch.parent.ROSLaunchParent(uuid, [X1_LAUNCH_URI])
         rospy.loginfo("[Launch-Selector] Launching X1....")
 
