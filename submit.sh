558111204db85a575163fc1458ee1e8a36422e93
Arjo <arjo129@gmail.com>
2020-09-03 10:17:28 +0800

    Automate submission

---
+++ submit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker tag arjo129/nusseds_subt_sol 200670743174.dkr.ecr.us-east-1.amazonaws.com/subt/nusseds:cave_circuit
+docker push 200670743174.dkr.ecr.us-east-1.amazonaws.com/subt/nusseds:cave_circuit
