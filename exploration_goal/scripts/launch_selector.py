675dd76cbded062af102436721988fb7aa47e65e
Arjo <arjo129@gmail.com>
2020-09-03 17:51:13 +0800

    made container friendly

--- exploration_goal/scripts/launch_selector.py
+++ exploration_goal/scripts/launch_selector.py
@@ -14,47 +14,58 @@ TOPIC_TIMEOUT = 10 # seconds
 SUBT_START_SRV_NAME = "/subt/start"
 
 CURR_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+LAUNCH_FILE_PATHS = CURR_PATH+"/launch"
 TEAMBASE_LAUNCH_URI = CURR_PATH + "/launch/teambase.launch"
-X1_LAUNCH_URI = CURR_PATH + "/launch/x1.launch"
-X2_LAUNCH_URI = CURR_PATH + "/launch/x2.launch"
+TEAMBASE_NAME= "teambase"
 launch = None
 
 def main():
     rospy.init_node('LaunchSelectorNode', anonymous=True)
-
+    rospy.logwarn("[Launch-Selector] Waiting for /subt/start service to start scoring...")
+    
+    rospy.wait_for_service(SUBT_START_SRV_NAME)
+    
+    # Start the scoring server
+    subt_start_srv = rospy.ServiceProxy(SUBT_START_SRV_NAME, SetBool)
+    start_req = SetBoolRequest()
+    start_req.data = True
+    subt_start_srv(start_req)
+    rospy.loginfo("[Launch-Selector] Subt Scoring server started")
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+    
 
-    try:
-        rospy.logwarn("[Launch-Selector] Waiting for message on {} for {} seconds \
-                before deciding to launch X1 or teambase".format(GUARANTEED_X1_TOPIC, TOPIC_TIMEOUT))
-        rospy.wait_for_message(GUARANTEED_X1_TOPIC, BatteryState, timeout=rospy.Duration(TOPIC_TIMEOUT))
+    #Get all publishe topics, map them to a corresponding launch file
+    # /X1/some_topic gets mapped to x1.launch
+    topics = rospy.get_published_topics()
 
-        # X1 topic found
+    launch_files = os.listdir(LAUNCH_FILE_PATHS)
 
-        ## Start Subt Scoring
-        rospy.logwarn("[Launch-Selector] Waiting for /subt/start service to start scoring...")
-        rospy.wait_for_service(SUBT_START_SRV_NAME)
-        subt_start_srv = rospy.ServiceProxy(SUBT_START_SRV_NAME, SetBool)
-        start_req = SetBoolRequest()
-        start_req.data = True
-        subt_start_srv(start_req)
-        rospy.loginfo("[Launch-Selector] Subt Scoring server started")
+    robot_to_launch = None
+    for file in launch_files:
+        fp = file.split(".")
+        if len(fp) != 2: continue
+        fname, ext = fp
+        if ext == "launch":
+            name = fname.upper()
+            for topic, msg_type in topics:
+                names = topic.split("/")
+                if name in names[:2]:
+                    robot_to_launch = fname
 
-        ## Start X1 launch
-        launch = roslaunch.parent.ROSLaunchParent(uuid, [X1_LAUNCH_URI, X2_LAUNCH_URI])
-        rospy.loginfo("[Launch-Selector] Launching X1 and X2....")
+    if robot_to_launch is None:
+        robot_to_launch = TEAMBASE_NAME
 
-    except ROSException:
-        # Could not find X1 topic, so it is likely teambase docker. using Teambase launch file
-        launch = roslaunch.parent.ROSLaunchParent(uuid, [TEAMBASE_LAUNCH_URI])
-        rospy.loginfo("[Launch-Selector] Launching Teambase....")
-
+    launch_file = os.path.join(LAUNCH_FILE_PATHS,robot_to_launch+".launch")
+    rospy.loginfo("launching robot file "+ launch_file)
+    
+    #Launch the 
+    launch = roslaunch.parent.ROSLaunchParent(uuid, [launch_file])
     rospy.sleep(2.0)
     launch.start()
     rospy.loginfo("[Launch-Selector] Launch Succesful. All tasks completed.")
 
     rospy.spin()
-
+    
 
 if __name__ == "__main__":
     main()
