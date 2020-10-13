74f0c51650638e2ccd773484dabde56e721f1a98
Arjo <arjo129@gmail.com>
2020-10-13 16:45:55 +0800

    simple rssi client server

--- exploration_goal/scripts/defecate.py
+++ exploration_goal/scripts/defecate.py
@@ -1,26 +1,58 @@
 #!/usr/bin/env python
 import rospy
+from tf.listener import TransformListener
 from noroute_mesh.srv import neighbour
 from std_msgs.msg import Empty
+from nav_msgs.msg import OccupancyGrid
+global rssi_reading
+rssi_reading = ""
 
+def last_rssi(message):
+    global rssi_reading
+    rssi_reading = message.header.frame_id
+    print "updating rssi"
+
+def parse_result(message):
+    neighbours_rssi_combined = res.response.split("|")
+    neighbours = {}
+    for n in neighbours_rssi_combined:
+        try:
+            neighbour, rssi= n.split(",")
+            if rssi != "inf":
+                neighbours[neighbour] = float(rssi)
+        except:
+            pass
+    return neighbours
+
+def determine_if_use_teambase(message):
+    global rssi_reading
+    print(rssi_reading, message)
+    neighbours_me = parse_result(rssi_reading)
+    print(neighbours_me)
+    neighbours_team_base = parse_result(message)
+    neighbours_me.update(neighbours_team_base)
+    return neighbours_me
+    
 if __name__ == "__main__":
     drop_points = [-55, -60, -65,  -70, -75, -78, -80, -82, -84, -85, -86]
     rospy.init_node("breadcrumb_dropper")
-    robot_name = rospy.get_param("~robot_name")
-    rospy.wait_for_service(robot_name+"/get_neighbour")
-    rate = rospy.Rate(1)
-    dropper = rospy.Publisher(robot_name+"/breadcrumb/deploy", Empty)
-
     try:
-        get_neighbours  = rospy.ServiceProxy(robot_name+'/get_neighbour', neighbour)
+        robot_name = rospy.get_param("~robot_name")
+    except:
+        robot_name= "X1"
+    print("waiting for comms service")
+    rate = rospy.Rate(1)
+    rate.sleep()
+    dropper = rospy.Publisher(robot_name+"/breadcrumb/deploy", Empty)
+    sub =rospy.Subscriber(robot_name+"/comms_publisher", OccupancyGrid, last_rssi)
+    
+    try:
+        get_neighbours  = rospy.ServiceProxy('X1/get_neighbour', neighbour)
         num_of_drops = 0
         while not rospy.is_shutdown():
             res = get_neighbours()
-            neighbours_rssi_combined = res.response.split("|")
-            neighbours = {}
-            for n in neighbours_rssi_combined:
-                neighbour, rssi= n.split(",")
-                neighbours[neighbour] = float(rssi) 
+            neighbours = determine_if_use_teambase(res.response)
+            print(neighbours)
             if "teambase" in neighbours:
                 #print(neighbours["teambase"])
                 rssi = neighbours["teambase"]
@@ -30,8 +62,8 @@ if __name__ == "__main__":
                     print ("dropping ", num_of_drops)
             else:
                 print ("LOST CONTACT")
-
+            rssi_reading = ""
             rate.sleep()
-    except:
-        print ("oops")
-        
+    except Exception as e:
+        print e
+    
---
+++ exploration_goal/scripts/robot_rssi_client.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import rospy
+from noroute_mesh.srv import neighbour
+from std_msgs.msg import Empty
+
