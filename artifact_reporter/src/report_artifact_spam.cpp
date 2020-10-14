f61f4b738742594b49e4fd671edab6ca553ac554
Hashir Zahir <hashirsahad@gmail.com>
2020-10-14 23:35:30 +0800

    Prevent nan reporting and check 2 in row for artifacts

--- artifact_reporter/src/report_artifact_spam.cpp
+++ artifact_reporter/src/report_artifact_spam.cpp
@@ -4,6 +4,7 @@
 #include "boost/ref.hpp"
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/PoseStamped.h"
+#include <cmath>
 
 #include "object_detection/DetectedObjectMsg.h"
 #include "object_detection/DetectedObjectsMsg.h"
@@ -11,6 +12,7 @@
 
 #define DETECTIONS_TOPIC "object_detector/detected"
 #define ARTIFACT_SRV_NAME "send_artifact"
+#define ARTIFACT_SIMILARITY_DIST 1.5 // meters
 
 class ArtifactReport {
 
@@ -40,11 +42,19 @@ public:
 
     void objectDetectionCb(const object_detection::DetectedObjectsMsg& msg)
     {
+        // Wait until previous detections have been reported to begin processing new detections
+        // if (have_an_artifact_to_report) {
+        //     return;
+        // }
+
         have_an_artifact_to_report = true;
 
         // for now only care about first detection
         auto detection = msg.detected_objects_msgs[0];
         std::string temp(detection.class_name.c_str());
+
+        std::string prev_artifact_type_str = artifact_type_str;
+        geometry_msgs::Point prev_artifact_location = artifact_location;
 
         if (temp == "backpack") {
             artifact_type_str = "TYPE_BACKPACK";
@@ -52,9 +62,12 @@ public:
             artifact_type_str = "TYPE_ROPE";
         } else if (temp == "helmet") {
             artifact_type_str = "TYPE_HELMET";
-        } else if (temp == "phone") {
-            artifact_type_str = "TYPE_PHONE";
-        } else if (temp == "survivor") {
+        } 
+        // Ignore phone as detector buggy
+        // else if (temp == "phone") {
+        //     artifact_type_str = "TYPE_PHONE";
+        // } 
+        else if (temp == "survivor") {
             artifact_type_str = "TYPE_RESCUE_RANDY";
         } else {
             ROS_ERROR("[Artifact-Report-Spam] Wrong class of artifact %s received. Discarded.", artifact_type_str.c_str());
@@ -66,6 +79,42 @@ public:
 
         artifact_location = detection.global_point.point;
 
+        // dont report nans
+        if (isArtifactLocationNan(artifact_location) || isArtifactLocationNan(prev_artifact_location)) {
+            ROS_ERROR("[Artifact-Report-Spam] NAN detection. artifact %s Discarded.", artifact_type_str.c_str());
+            have_an_artifact_to_report = false;
+            return;
+        }
+
+        // dont report if previous and current detections are not the "same"
+        if (!isArtifactReportSimilar(prev_artifact_type_str, artifact_type_str, prev_artifact_location, artifact_location)) {
+            ROS_ERROR("[Artifact-Report-Spam] Not 2 consecutive similar detections. artifact %s Discarded.", artifact_type_str.c_str());
+            have_an_artifact_to_report = false;
+            return;
+        } 
+
+    }
+
+    bool isArtifactLocationNan(geometry_msgs::Point location) {
+        return (std::isnan(artifact_location.x) || std::isnan(artifact_location.y) || std::isnan(artifact_location.z));
+    }
+
+    bool isArtifactReportSimilar(std::string prev_artifact_type_str, std::string artifact_type_str,
+                                geometry_msgs::Point prev_artifact_location, geometry_msgs::Point artifact_location) {
+        
+        if (prev_artifact_type_str != artifact_type_str) {
+            return false;
+        }
+
+        if (getDist(prev_artifact_location,artifact_location) > ARTIFACT_SIMILARITY_DIST) {
+            return false;
+        }
+
+        return true;
+    }
+
+    double getDist(geometry_msgs::Point point1, geometry_msgs::Point point2) {
+        return std::sqrt(std::pow(point1.x-point2.x, 2) + std::pow(point1.y-point2.y, 2) + std::pow(point1.z-point2.z, 2));
     }
 
     void reportArtifacts(const ros::TimerEvent &)
