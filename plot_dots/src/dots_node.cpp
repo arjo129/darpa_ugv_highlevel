1c0b81df56d9f2292fbe2836d3ba4be7480833f0
ngmysamuel <43942347+ngmysamuel@users.noreply.github.com>
2020-12-29 09:15:57 +0800

    Add edges. See more in description.
    Edges added, will be published in the /global_graph/vis topic which makes use of /output2. You will need to edit exploration_goal.launch file in exploration_goal package - change the value for the param "graph_topic" to "output2". Then run "roslaunch exploration_goal exploration_goal.launch robot_name:=X1"
    Max z height is set to 15. Resolution of dots is set to 1.5. Open the source code to change them.
     ~0.6s - 0.9s
    amapper, exploration_goal, graph_msgs, map_merge, plot_dots and subt_msgs is required for building plot_dots.

--- plot_dots/src/dots_node.cpp
+++ plot_dots/src/dots_node.cpp
@@ -19,151 +19,186 @@ I will also call add_to_queue which looks at the possible dots around it in 3 di
 #include <iostream>
 #include <list>
 #include <unordered_map>
-#include <tuple>
+//#include <tuple>
 #include <queue> 
 
 #include <boost/functional/hash.hpp>
 #include <math.h> 
+#include <time.h>
+
+#include <graph_msgs/Edges.h>
+#include <graph_msgs/GeometryGraph.h>
+#include "geometry_msgs/Point.h"
 
 ros::Publisher pub;
-const float dot_distance = 3;
+ros::Publisher pub2;
+const float dot_distance = 1.5;
+const float max_z = 15;
 const float EPSILON = 0.0001;
 
 struct key_hash {
-  std::size_t operator()(const std::tuple<float, float, float>& k) const {
+  std::size_t operator()(const pcl::PointXYZ& k) const {
       using boost::hash_value;
       using boost::hash_combine;
 
       // Start with a hash value of 0    .
       std::size_t seed = 0;
 
-      //hash_combine(seed,hash_value(k.first));
-	  //hash_combine(seed,hash_value(k.second));
-      //hash_combine(seed,hash_value(k.third));
-	  hash_combine(seed,hash_value(std::get<0>(k)));
-	  hash_combine(seed,hash_value(std::get<1>(k)));
-	  hash_combine(seed,hash_value(std::get<2>(k)));
+	  //hash_combine(seed,hash_value(std::get<0>(k)));
+	  //hash_combine(seed,hash_value(std::get<1>(k)));
+	  //hash_combine(seed,hash_value(std::get<2>(k)));
+	  hash_combine(seed,hash_value(k.x));
+	  hash_combine(seed,hash_value(k.y));
+	  hash_combine(seed,hash_value(k.z));
 
       // Return the result.
       return seed;
   }
 };
 
-struct key_equal : public std::binary_function<std::tuple<float, float, float>, std::tuple<float, float, float>, bool> {
-  bool operator()(const std::tuple<float, float, float>& v0, const std::tuple<float, float, float>& v1) const {
+struct key_equal : public std::binary_function<pcl::PointXYZ, pcl::PointXYZ, bool> {
+  bool operator()(const pcl::PointXYZ& v0, const pcl::PointXYZ& v1) const {
    return (
-   fabs(std::get<0>(v0) - std::get<0>(v1)) < EPSILON &&
-   fabs(std::get<1>(v0) - std::get<1>(v1)) < EPSILON &&
-   fabs(std::get<2>(v0) - std::get<2>(v1)) < EPSILON
+   fabs(v0.x - v1.x) < EPSILON &&
+   fabs(v0.y - v1.y) < EPSILON &&
+   fabs(v0.z - v1.z) < EPSILON
   );
   }
 };
 
 
-//standardise to making use of pcl::pointXYZ only instead of std::tuple as KEY in the map
 
-void add_to_queue(std::unordered_map<std::tuple<float, float, float>,bool,key_hash,key_equal>& visited_map, pcl::PointXYZ& point,std::queue<pcl::PointXYZ>& q, LidarScan& scan) 
+int add_to_queue(std::unordered_map<pcl::PointXYZ,int,key_hash,key_equal>& visited_map, pcl::PointXYZ& point,std::queue<pcl::PointXYZ>& q, LidarScan& scan, graph_msgs::Edges& e, int& index) 
 {
-	std::tuple<float, float, float> tempUp, tempDown, tempLeft, tempRight, tempForward, tempBackward;
+	//std::tuple<float, float, float> tempUp, tempDown, tempLeft, tempRight, tempForward, tempBackward;
 	pcl::PointXYZ pointUp, pointDown, pointLeft, pointRight, pointForward, pointBackward;
+	std::unordered_map<pcl::PointXYZ, int, key_hash, key_equal>::iterator search;
+	//std::cout << "Index 1: " << index << std::endl;
 	
-	std::get<0>(tempUp) = point.x;
-	std::get<1>(tempUp) = point.y;
-	std::get<2>(tempUp) = point.z + dot_distance;
 	pointUp.x = point.x;
 	pointUp.y  = point.y;
 	pointUp.z  = point.z + dot_distance;	
-	auto search = visited_map.find(tempUp);
-	if (search == visited_map.end() && isPointInside(scan, pointUp)) { //did NOT manage to find it in the map, therefore is NOT visited
-		std::cout << "pushed into queue Down: " << point.x << ", " << point.y << ", " << point.z + dot_distance << std::endl;	
-		q.push(pointUp); //add it to the queue
-		visited_map.insert({tempUp, true}); //add it to visited points
-	} else {
-		//std::cout << "FOUND INSIDE OF MAP" << std::endl;
+	if (pointUp.z < max_z) {
+		search = visited_map.find(pointUp);
+		if (isPointInside(scan, pointUp)) { //if neighbouring point is still inside of scan
+			if (search == visited_map.end()) { //if have not visited the point yet, add it to the queue to visit
+				//std::cout << "pushed into queue Up: " << pointUp.x << ", " << pointUp.y << ", " << pointUp.z << std::endl;	
+				++index;
+				e.node_ids.push_back(index); //index should be referring to pointUp
+				q.push(pointUp); //add it to the queue
+				visited_map.insert({pointUp, index}); //add it to visited points
+			} else { //have visited this neighbouring point before
+				//std::cout << "have visited this neighbouring point before: " << pointUp.x << ", " << pointUp.y << ", " << pointUp.z << std::endl;	
+				e.node_ids.push_back(visited_map[pointUp]);
+			}
+		}
 	}
 
-	
-	std::get<0>(tempDown) = point.x;
-	std::get<1>(tempDown) = point.y;
-	std::get<2>(tempDown) = point.z - dot_distance;
 	pointDown.x = point.x;
 	pointDown.y  = point.y;
 	pointDown.z  = point.z - dot_distance;
-	search = visited_map.find(tempDown);
-	if (search == visited_map.end() && isPointInside(scan, pointDown)) { //did not manage to find it in the map, therefore is not visited
-		std::cout << "pushed into queue Down: " << point.x << ", " << point.y << ", " << point.z - dot_distance << std::endl;	
-		q.push(pointDown); //add it to the queue
-		visited_map.insert({tempDown, true}); //add it to visited points
-	}  else {
-		//std::cout << "FOUND INSIDE OF MAP" << std::endl;
+	if (pointDown.z < max_z) {
+		search = visited_map.find(pointDown);
+		if ( isPointInside(scan, pointDown)) { //if neighbouring point is still inside of scan
+			if (search == visited_map.end() ) { //if have not visited the point yet, add it to the queue to visit
+				//std::cout << "pushed into queue Down: " << pointDown.x << ", " << pointDown.y << ", " << pointDown.z  << std::endl;	
+				++index;
+				e.node_ids.push_back(index);
+				q.push(pointDown); //add it to the queue
+				visited_map.insert({pointDown, index}); //add it to visited points
+			}  else {
+				//std::cout << "have visited this neighbouring point before: " << pointDown.x << ", " << pointDown.y << ", " << pointDown.z << std::endl;	
+				e.node_ids.push_back(visited_map[pointDown]);
+			}
+		}
 	}
 
 	
-	std::get<0>(tempLeft) = point.x;
-	std::get<1>(tempLeft) = point.y - dot_distance;
-	std::get<2>(tempLeft) = point.z;
 	pointLeft.x = point.x;
 	pointLeft.y  = point.y - dot_distance;
 	pointLeft.z  = point.z;	
-	search = visited_map.find(tempLeft);
-	if (search == visited_map.end() && isPointInside(scan, pointLeft)) { //did not manage to find it in the map, therefore is not visited
-		std::cout << "pushed into queue Left: " << point.x << ", " << point.y - dot_distance << ", " << point.z << std::endl;	
-		q.push(pointLeft); //add it to the queue
-		visited_map.insert({tempLeft, true}); //add it to visited points
-	}  else {
-		//std::cout << "FOUND INSIDE OF MAP" << std::endl;
+	if (pointLeft.z < max_z) {		
+		search = visited_map.find(pointLeft);
+		if (isPointInside(scan, pointLeft)) {
+			if (search == visited_map.end()) { 
+				//std::cout << "pushed into queue Left: " << pointLeft.x << ", " << pointLeft.y << ", " << pointLeft.z << std::endl;	
+				++index;
+				e.node_ids.push_back(index);
+				q.push(pointLeft); //add it to the queue
+				visited_map.insert({pointLeft, index}); //add it to visited points
+			} else {
+				//std::cout << "have visited this neighbouring point before: " << pointLeft.x << ", " << pointLeft.y << ", " << pointLeft.z << std::endl;	
+				e.node_ids.push_back(visited_map[pointLeft]);
+			}
+		}
 	}
 
 	
-	std::get<0>(tempRight) = point.x;
-	std::get<1>(tempRight) = point.y + dot_distance;
-	std::get<2>(tempRight) = point.z;
 	pointRight.x = point.x;
 	pointRight.y  = point.y + dot_distance;
 	pointRight.z  = point.z;
-	search = visited_map.find(tempRight);
-	if (search == visited_map.end() && isPointInside(scan, pointRight)) { //did not manage to find it in the map, therefore is not visited
-		std::cout << "pushed into queue Right: " << point.x << ", " << point.y + dot_distance << ", " << point.z << std::endl;	
-		q.push(pointRight); //add it to the queue
-		visited_map.insert({tempRight, true}); //add it to visited points
-	}  else {
-		//std::cout << "FOUND INSIDE OF MAP" << std::endl;
+	if (pointRight.z < max_z) {		
+		search = visited_map.find(pointRight);
+		if (isPointInside(scan, pointRight)) {
+			if (search == visited_map.end()) { 
+				//std::cout << "pushed into queue Right: " << pointRight.x << ", " << pointRight.y << ", " << pointRight.z << std::endl;	
+				++index;
+				e.node_ids.push_back(index);
+				q.push(pointRight); //add it to the queue
+				visited_map.insert({pointRight, index}); //add it to visited points
+			}  else {
+				//std::cout << "have visited this neighbouring point before: " << pointRight.x << ", " << pointRight.y << ", " << pointRight.z << std::endl;	
+				e.node_ids.push_back(visited_map[pointRight]);
+			}
+		}
 	}
 	
-	std::get<0>(tempForward) = point.x + dot_distance;
-	std::get<1>(tempForward) = point.y;
-	std::get<2>(tempForward) = point.z;
+	
 	pointForward.x = point.x + dot_distance;
 	pointForward.y  = point.y;
 	pointForward.z  = point.z;
-	search = visited_map.find(tempForward);
-	if (search == visited_map.end() && isPointInside(scan, pointForward)) { //did not manage to find it in the map, therefore is not visited
-		std::cout << "pushed into queue Forward: " << point.x + dot_distance << ", " << point.y << ", " << point.z << std::endl;	
-		q.push(pointForward); //add it to the queue
-		visited_map.insert({tempForward, true}); //add it to visited points
-	}  else {
-		//std::cout << "FOUND INSIDE OF MAP" << std::endl;
+	if (pointForward.z < max_z) {			
+		search = visited_map.find(pointForward);
+		if (isPointInside(scan, pointForward)) {
+			if (search == visited_map.end()) { 
+				//std::cout << "pushed into queue Forward: " << pointForward.x << ", " << pointForward.y << ", " << pointForward.z << std::endl;	
+				++index;
+				e.node_ids.push_back(index);
+				q.push(pointForward);
+				visited_map.insert({pointForward, index});
+			} else {
+				//std::cout << "have visited this neighbouring point before: " << pointForward.x << ", " << pointForward.y << ", " << pointForward.z << std::endl;	
+				e.node_ids.push_back(visited_map[pointForward]);
+			}
+		}
 	}
 	
-	std::get<0>(tempBackward) = point.x - dot_distance;
-	std::get<1>(tempBackward) = point.y;
-	std::get<2>(tempBackward) = point.z;
+
 	pointBackward.x = point.x - dot_distance;
 	pointBackward.y  = point.y;
 	pointBackward.z  = point.z;
-	search = visited_map.find(tempBackward);
-	if (search == visited_map.end() && isPointInside(scan, pointBackward)) { //did not manage to find it in the map, therefore is not visited
-		std::cout << "pushed into queue Back: " << point.x - dot_distance << ", " << point.y << ", " << point.z << std::endl;	
-		q.push(pointBackward); //add it to the queue
-		visited_map.insert({tempBackward, true}); //add it to visited points
-	} 	 else {
-		//std::cout << "FOUND INSIDE OF MAP" << std::endl;
+	if (pointBackward.z < max_z) {
+		search = visited_map.find(pointBackward);
+		if (isPointInside(scan, pointBackward)) {
+			if (search == visited_map.end()) {
+				//std::cout << "pushed into queue Back: " << pointBackward.x << ", " << pointBackward.y << ", " << pointBackward.z << std::endl;	
+				++index;
+				e.node_ids.push_back(index);
+				q.push(pointBackward); //add it to the queue
+				visited_map.insert({pointBackward, index}); //add it to visited points
+			} 	 else {
+				//std::cout << "have visited this neighbouring point before: " << pointBackward.x << ", " << pointBackward.y << ", " << pointBackward.z << std::endl;	
+				e.node_ids.push_back(visited_map[pointBackward]);
+			}
+		}
 	}
-
+	
+	//std::cout << "Index 2: " << index << std::endl;
 }
 
 void cloud_cb(const sensor_msgs::PointCloud2& cloud_msg) {
-	std::cout << "start" << std::endl;
+	clock_t tStart = clock();
+	//std::cout << "start" << std::endl;
 	pcl::PCLPointCloud2 pcl_cloud_msg_2;
 	pcl_conversions::toPCL(cloud_msg, pcl_cloud_msg_2);
 	pcl::PointCloud<pcl::PointXYZ> pcl_cloud_msg;
@@ -175,33 +210,53 @@ void cloud_cb(const sensor_msgs::PointCloud2& cloud_msg) {
 	
 	pcl::PointXYZ temp;
 	pcl::PointXYZ start = pcl::PointXYZ(0,0,0);
-	std::unordered_map<std::tuple<float, float, float>, bool, key_hash, key_equal> visited_map;
-	//std::unordered_map<std::tuple, bool> visited_map;
+	std::unordered_map<pcl::PointXYZ, int, key_hash, key_equal> visited_map;
 	
 	pcl::PointCloud<pcl::PointXYZ> out_cloud;
 	std::queue<pcl::PointXYZ> q;
 	
+	graph_msgs::GeometryGraph out_cloud_2;
+	static geometry_msgs::Point out_cloud_2_point;
+	int index = -1;
+	
 	q.push(start);
+	++index;
+	pcl::PointXYZ start_point;
+	start_point.x = 0;
+	start_point.y = 0;
+	start_point.z = 0;
+	visited_map.insert({start_point, index}); //add it to visited points
 	
 	while (!q.empty()) {
+		//std::cout << "Index 0: " << index << std::endl;
 		//std::cout << "Queue size 0: " << q.size() << std::endl;
 		
 		temp = q.front();
 		q.pop();
 		//std::cout << "Queue size 1: " << q.size() << std::endl;
-	
 		out_cloud.push_back (temp); //add it to the cloud to be publish
-		add_to_queue(visited_map, temp, q, scan); //add points around temp to the queue
 		
-		//std::cout << "Queue size 2:  " << q.size() << std::endl;
+		out_cloud_2_point.x = temp.x;
+		out_cloud_2_point.y = temp.y;
+		out_cloud_2_point.z = temp.z;
+		out_cloud_2.nodes.push_back(out_cloud_2_point);
+		graph_msgs::Edges e;
+		add_to_queue(visited_map, temp, q, scan, e, index); //add points around temp to the queue
+		out_cloud_2.edges.push_back(e);		
+
 	}
 	
+	std::vector<uint8_t> arr(out_cloud_2.nodes.size());
+	//std::cout<< out_cloud_2.nodes.size() << "\n"; //number of points
+	out_cloud_2.explored = arr;
 	
-//	std::cout << scan.size() << std::endl;
-	out_cloud.header.frame_id = "X1/base_link";
-	out_cloud.header.stamp = pcl_conversions::toPCL(cloud_msg.header.stamp);
-	pub.publish(out_cloud);
-	std::cout << "OVER" << std::endl;
+	//out_cloud.header.frame_id = "X1/base_link";
+	//out_cloud.header.stamp = pcl_conversions::toPCL(cloud_msg.header.stamp);
+	out_cloud_2.header = cloud_msg.header;
+	//pub.publish(out_cloud);
+	pub2.publish(out_cloud_2);
+	//std::cout << "OVER" << std::endl;
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 	//ros::shutdown();
 }
 
@@ -210,6 +265,7 @@ int main (int argc, char* argv[]) {
 	ros::NodeHandle nh;
 	ros::Subscriber sub = nh.subscribe ("/X1/points/", 1, cloud_cb);
 	//pub = nh.advertise<sensor_msgs::PointCloud> ("output", 1);
-	pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>> ("output", 1);	
+	//pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>> ("output", 1);	
+	pub2 = nh.advertise<graph_msgs::GeometryGraph> ("output2", 1);	
 	ros::spin();
 }
