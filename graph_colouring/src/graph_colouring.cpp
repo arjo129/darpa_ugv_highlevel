f075e6ea281abd12af4c9628bf62a825363f82d3
kronicler <omnikron96@gmail.com>
2021-06-01 08:11:54 +0800

    added transformListener

--- graph_colouring/src/graph_colouring.cpp
+++ graph_colouring/src/graph_colouring.cpp
@@ -3,7 +3,9 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_geometry/pinhole_camera_model.h>
-#include <tf/transform_listener.h>
+#include "tf2/buffer.h"
+#include <tf2/transform_listener.h>
+#include <tf2/transform_broadcaster.h>
 #include <boost/foreach.hpp>
 #include <sensor_msgs/image_encodings.h>
 #include <geometry_msgs/Point.h>
@@ -20,52 +22,52 @@ class graph_colouring_tool{
 
     image_transport::ImageTransport it_;
     image_geometry::PinholeCameraModel cam_model;
+
+    tf2::Buffer tfBuffer;
+    tf2::TransformListener tfListener(tfBuffer);
+    tf2::TransformBroadcaster br;
+
+    geometry_msgs::TransformStamped transformStamped;
+
     cv_bridge::CvImagePtr input_bridge;
     cv::Mat image;
     cv::Point3d **arr;
 
     public:
     graph_colouring_tool(): it_(nh_){
-        std::string image_topic = nh_.resolveName("/X1/front/optical/depth");
+        std::string image_topic = nh_.resolveName("/X1/front/optical/image_raw");
         sub_ = it_.subscribeCamera(image_topic, 1, &graph_colouring_tool::image_cb, this);
         pub_ = it_.advertise("image_out", 1);
+        get_vectors();
     }
 
     void image_cb(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info_msg){
-        cv::Mat rawImage, rectifiedImage;
-        cv_bridge::CvImagePtr input_bridge;
-        // try {
-        //     input_bridge = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
-        //     image = input_bridge->image;
-        // }
-        // catch (cv_bridge::Exception& ex){
-        //     ROS_ERROR("[draw_frames] Failed to convert image");
-        //     return;
-        // }
         cam_model.fromCameraInfo(info_msg);
+        //printf("%s\n", cam_model.tfFrame());
+        
+        try{
+            transformedStamped = tfBuffer.lookupTransform(cam_model.tfFrame(),"world",ros::Time(0));
+        }
+        catch(tf2::TransformException &ex) {
+            ROS_WARN("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
 
-        cv::Size s;
-        s = cam_model.fullResolution();
-        printf("%d, %d\n",s.width,s.height);
-        //get_vectors();
-
-        // cam_model.rectifyImage(rawImage, rectifiedImage);
-        // input_bridge->image = rectifiedImage;
-        // pub_.publish(input_bridge->toImageMsg());
-        printf("cx: %d, cy: %d\n", cam_model.cx(),cam_model.cy());
-        //printf("CAPTURED\n");
     }
     
     void get_vectors(){
         cv::Point3d temp;
-        for (int i=0; i<HEIGHT; i++){
-            for (int j=0; j<WIDTH;j++){
+        for (double i=0; i<HEIGHT; i++){
+            for (double j=0; j<WIDTH;j++){
                 cv::Point2d uv(i,j);
                 temp = cam_model.projectPixelTo3dRay(cam_model.rectifyPoint(uv));
-                printf("i:%d j:%d | x:%d y:%d z:%d\n", i, j, temp.x, temp.y, temp.z);
+                if (i== 0.0 && j == 0.0)
+                printf("i:%lf j:%lf | x:%lf y:%lf z:%lf\n", i, j, temp.x, temp.y, temp.z);
             }
         }
     }
+
 };
 
 int main(int argc, char** argv)
