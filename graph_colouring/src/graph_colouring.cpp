7cb6ea5a511b63550bfee67057c34194f69ac4ce
kronicler <omnikron96@gmail.com>
2021-06-13 11:05:34 +0800

    tf2 transform not working

--- graph_colouring/src/graph_colouring.cpp
+++ graph_colouring/src/graph_colouring.cpp
@@ -3,9 +3,9 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_geometry/pinhole_camera_model.h>
-#include "tf2/buffer.h"
-#include <tf2/transform_listener.h>
-#include <tf2/transform_broadcaster.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
 #include <boost/foreach.hpp>
 #include <sensor_msgs/image_encodings.h>
 #include <geometry_msgs/Point.h>
@@ -18,42 +18,46 @@ class graph_colouring_tool{
     ros::NodeHandle nh_;
     image_transport::CameraSubscriber sub_;
     image_transport::Publisher pub_;
-    // ros::Subscriber sub_;
+    
+    tf2_ros::Buffer tfBuffer_;
+    tf2_ros::TransformBroadcaster br;
+    tf2_ros::TransformListener* ls;
 
     image_transport::ImageTransport it_;
     image_geometry::PinholeCameraModel cam_model;
-
-    tf2::Buffer tfBuffer;
-    tf2::TransformListener tfListener(tfBuffer);
-    tf2::TransformBroadcaster br;
 
     geometry_msgs::TransformStamped transformStamped;
 
     cv_bridge::CvImagePtr input_bridge;
     cv::Mat image;
-    cv::Point3d **arr;
+
+    std::vector<cv::Point3d> arr;
 
     public:
     graph_colouring_tool(): it_(nh_){
         std::string image_topic = nh_.resolveName("/X1/front/optical/image_raw");
         sub_ = it_.subscribeCamera(image_topic, 1, &graph_colouring_tool::image_cb, this);
         pub_ = it_.advertise("image_out", 1);
+        ls = new tf2_ros::TransformListener(tfBuffer_);
         get_vectors();
     }
 
     void image_cb(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info_msg){
         cam_model.fromCameraInfo(info_msg);
         //printf("%s\n", cam_model.tfFrame());
-        
+        tf2::Vector3 temp,test;
+        temp.setX(1.0);
+        temp.setY(1.0);
+        temp.setZ(1.0);
+        temp.setW(1.0);
+        // transform all the saved points;
         try{
-            transformedStamped = tfBuffer.lookupTransform(cam_model.tfFrame(),"world",ros::Time(0));
+            transformStamped = tfBuffer_.lookupTransform(cam_model.tfFrame(), "world", ros::Time(0));
+            test = tfBuffer_.transform(temp,"world");
+            printf("x:%lf y:%lf z:%lf\n",test.x(), test.y(), test.z());
+        } catch (tf2::TransformException &ex) {
+            ROS_WARN("Could NOT transform camera to world frame: %s", ex.what());
         }
-        catch(tf2::TransformException &ex) {
-            ROS_WARN("%s",ex.what());
-            ros::Duration(1.0).sleep();
-            continue;
-        }
-
     }
     
     void get_vectors(){
@@ -62,8 +66,8 @@ class graph_colouring_tool{
             for (double j=0; j<WIDTH;j++){
                 cv::Point2d uv(i,j);
                 temp = cam_model.projectPixelTo3dRay(cam_model.rectifyPoint(uv));
-                if (i== 0.0 && j == 0.0)
                 printf("i:%lf j:%lf | x:%lf y:%lf z:%lf\n", i, j, temp.x, temp.y, temp.z);
+                arr.push_back(temp);
             }
         }
     }
