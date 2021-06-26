a019baab37ace8bd876339880faf204f75b31c34
nyangshawbin <nyangshawbin@gmail.com>
2021-06-27 00:18:04 +0800

    fixed path in docker

--- exploration_goal/src/get_ur_ass_inside_cave.cpp
+++ exploration_goal/src/get_ur_ass_inside_cave.cpp
@@ -43,6 +43,54 @@ bool last_waypoint_completed = false;
 std::vector <std::pair<float,float>> waypoints;
 int waypoint_index = 0;
 
+// void onFrontierAvailable(const sensor_msgs::PointCloud2::Ptr frontiers_ptr) {
+//     if(!first || !obstacle_avoidance_ready) return;
+
+//     ROS_INFO("Got frontier message");
+//     pcl::PointCloud<pcl::PointXYZ> frontier;
+//     pcl::fromROSMsg(*frontiers_ptr, frontier);    
+//     tf::StampedTransform world_to_baselink;
+//     try{
+//         listener->waitForTransform(robot_name + "/base_link", robot_name+"/world", frontiers_ptr->header.stamp, ros::Duration(10));
+//         listener->lookupTransform(robot_name + "/base_link", robot_name+"/world", frontiers_ptr->header.stamp, world_to_baselink);
+//     } catch (tf::LookupException e){
+//         ROS_ERROR("failed to lookup transform: %s", e.what());
+//         return;
+//     }
+
+//     //auto status = ros::topic::waitForMessage<std_msgs::Int8>("status", node);
+
+//     subt_msgs::PoseFromArtifact srv;
+//     srv.request.robot_name.data = robot_name;
+
+//     if(client.call(srv)) {
+//         auto darpa_to_base_link = invertTransform(srv);
+//         auto dir = darpa_to_base_link.getOrigin();
+//         std::cout << "Got artifact_origin as" << dir.x() << "," << dir.y() << std::endl;
+//         float max_dot = 0;
+
+//         for(auto point: frontier) {
+//             tf::Vector3 vec(point.x, point.y, point.z);
+//             auto l = vec.dot(dir);
+//             if(l > max_dot){
+//                 max_dot = l;
+//                 goal = vec;
+//             }
+//         }
+//         std::cout << "Setting goal" <<goal.x() <<"," <<goal.y() <<std::endl;
+//         first =false;
+//         geometry_msgs::PointStamped stamp;
+//         stamp.header.frame_id = robot_name + "world";
+//         stamp.header.stamp = ros::Time::now();
+//         stamp.point.x = 0.9*goal.x();
+//         stamp.point.y = 0.9*goal.y();
+//         stamp.point.z = 0.9*goal.z();
+//         exploration_goal_pub.publish(stamp);
+//         goal_set = true;
+//     }
+    
+// }
+
 void create_path(){
   waypoints.push_back (std::make_pair (10,-4));
   waypoints.push_back (std::make_pair (15,-4));
@@ -186,7 +234,13 @@ void test(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg){
     std::cout << "current pose: " << goal.x() << " " << goal.y() << std::endl;
     std::cout << "distance to goal: " << distance << std::endl;
 
-    if (new_goal && !last_waypoint_completed){
+    // if (new_goal && !last_waypoint_completed){
+    //   new_goal = false;
+    //   exploration_goal_pub.publish(goal_msg_gl);  
+    //   std::cout << "Publishing " << waypoint_index << " th waypoint:" << waypoints[waypoint_index].first << " " << waypoints[waypoint_index].second << std:: endl;
+    // } 
+  
+    if (!last_waypoint_completed){
       new_goal = false;
       exploration_goal_pub.publish(goal_msg_gl);  
       std::cout << "Publishing " << waypoint_index << " th waypoint:" << waypoints[waypoint_index].first << " " << waypoints[waypoint_index].second << std:: endl;
@@ -217,7 +271,12 @@ int main(int argc, char** argv) {
     listener = new tf::TransformListener();
     
     create_path();
-
+    // ros::Rate r(10);
+    // while (ros::ok){
+    //   pub_path();
+    //   // ros::spinOnce();
+    //   // r.sleep();
+    // }
     ros::Rate r(rate);
     // while (ros::ok()){
     //   pub_path();
